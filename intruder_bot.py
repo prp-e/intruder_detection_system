77cda5c3f1b1f44b87c6d13432ac8a45e70407ad
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-05-12 22:36:36 +0430

    Let's work on this bitch

--- intruder_bot.py
+++ intruder_bot.py
@@ -1,5 +1,17 @@
 import os
 import requests
+import time 
 
-tg_token = os.getenv("BOT_API")
-requests.get(f"https://tg.kootahkon.ir/bot{tg_token}/sendMessage?chat_id=295600320&text=Intruder entered the room")
+
+#res = requests.get(f"https://tg.kootahkon.ir/bot{tg_token}/sendPhoto?chat_id=295600320&photo=https://9497-2a01-5ec0-e001-adbe-216a-285b-14ce-2cdd.ngrok.io/{picname}.jpg")
+before = before = dict ([(f, None) for f in os.listdir ('activities')])
+print(before)
+
+while True:
+    time.sleep(2)
+    after = dict ([(f, None) for f in os.listdir ('activities')])
+    added = [f for f in after if not f in before]
+    if len(added) > 0:
+        added = added.sort()
+        print(added[0])
+    before = after
