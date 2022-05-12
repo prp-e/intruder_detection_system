3bb3c83deb1e271a7fa06d53a06a1eca095cb2d3
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-05-12 23:02:11 +0430

    Bigger time intervals

--- intruder_bot.py
+++ intruder_bot.py
@@ -15,7 +15,7 @@ class Watcher:
         self.observer.start()
         try:
             while True:
-                time.sleep(1)
+                time.sleep(5)
         except:
             self.observer.stop()
             print("Error")
